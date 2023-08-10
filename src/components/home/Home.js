0d4a6d12f772b639d33f3ae80c7ff3d99e79e828
Hoanghuyen2k3 <khanhhuyenx20@gmail.com>
2023-08-10 20:25:09 +0700

    first commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        Welcome to VNhub: Your Gateway to Experiencing Vietnam's Vibrant Culture and Delights!
+
+Discover Vietnam's Rich Tapestry of Festivals, Cuisine, and Captivating Destinations
+
+Embark on a journey through the heart of Vietnam's cultural treasures with VNhub. Immerse yourself in a world where age-old traditions meet modern excitement, where every corner tells a story, and where every dish is a masterpiece.
+
+Festivals that Ignite the Spirit:
+Experience the pulse of Vietnam through its captivating festivals. From the electrifying lantern spectacle of the Mid-Autumn Festival in Hoi An to the colorful pageantry of Tet Nguyen Dan (Lunar New Year), our festivals resonate with centuries of history and unbridled joy. Witness dragon dances, floral displays, and join locals in their traditional celebrations, creating memories that last a lifetime.
+
+Culinary Odyssey Beyond Compare:
+Vietnamese cuisine is a symphony of flavors, a delicate balance of sweet, savory, and tangy notes that awaken the senses. Savor the iconic Pho, a soul-soothing noodle soup, or dive into the crispy freshness of Goi Cuon (spring rolls). Our culinary adventure spans bustling street markets to elegant restaurants, where you'll learn the art of crafting these delectable dishes yourself.
+
+Captivating Destinations Aplenty:
+Whether you're captivated by the charm of ancient Hanoi's Old Quarter, entranced by the ethereal beauty of Ha Long Bay's emerald waters, or intrigued by the bustling energy of Ho Chi Minh City, VNhub guides you to Vietnam's most enchanting destinations. Trek through the terraced fields of Sapa, cruise along the Mekong Delta's intricate waterways, and wander the historic streets of Hoi An – each step is an exploration of wonder.
+
+Why VNhub:
+
+Local Insights: Our curated experiences are designed by locals, ensuring an authentic and immersive encounter with Vietnam's cultural gems.
+Tailored Itineraries: Craft your journey according to your preferences, be it a culinary escapade, festival immersion, or leisurely exploration.
+Expert Guides: Our knowledgeable guides are passionate storytellers, adding depth to every place you visit.
+Unforgettable Memories: From sunrise at Angkor Wat to savoring street food under the stars, VNhub creates moments that become cherished memories.
+Book Your Adventure Today!
+Let VNhub be your passport to the heart and soul of Vietnam. Whether you're an adventurous explorer, a culture enthusiast, or a culinary connoisseur, there's a world of experiences waiting for you. Begin your journey with VNhub today and unlock the secrets of Vietnam's festivals, food, and fascinating destinations.
+
+Your Adventure Awaits. Your Memories Begin. Explore Vietnam with VNhub.
+
+    </div>
+  )
+}
+
+export default Home
